02867f2e9d0d36d83160fd132cacfd552a9c077c
suryaprasathgss <154334150+suryaprasathgss@users.noreply.github.com>
2025-07-26 21:00:48 +0530

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-from datetime import datetime, timedelta
-
-todate = datetime.now().strftime("%Y-%m-%d")
-
-# Calculate yesterday's date
-yesterday = datetime.now() - timedelta(days=1)
-
-print(yesterday)
